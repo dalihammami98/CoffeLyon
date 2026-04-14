0dcd0e79b7a9ea99b5f72034fb2216fd65695ce2
dalihammami98 <mohamedalihammami98@gmail.com>
2026-04-14 01:47:54 +0100

    Refactor swiper and menu interaction code

--- main.js
+++ main.js
@@ -21,12 +21,4 @@ var swiper = new Swiper(".mySwiper", {
       menu.classList.remove("move");
       navbar.classList.remove("open-menu");
     };
-    const animate = ScrollReveal({
-      origin: "top",
-      distance: "60px",
-      duration: "2500",
-      delay: "400",
-    });
-    animate.reveal(".home-text", { origin: "left"});
-    animate.reveal(".home-img", { origin: "bottom"});
-    animate.reveal(".ser-box, .product-box, .team-box, .book-data", { interval: 100});
+   
